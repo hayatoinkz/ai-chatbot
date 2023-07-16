5313a7e43bdb84a6584be8da12d3c0cdc3ea82d1
Vinicius Rocha <vinicius.workspace@gmail.com>
2023-07-16 00:45:22 -0300

    refactor: add chat component with vercel hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,9 @@
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
+import { Chat } from '@/components/Chat';
 
 export default function Home() {
   return (
     <div className="flex min-h-screen bg-slate-50 items-center justify-center">
-      <Card className="w-[440px] min-h-[568px] grid grid-rows-[min-content_1fr_min-content]">
-        <CardHeader>
-          <CardTitle>Chat AI</CardTitle>
-          <CardDescription>
-            Using Vercel SDK to create a chat bot.
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="space-y-4">
-          <div className="flex gap-3 text-slate-800 text-sm">
-            <Avatar>
-              <AvatarFallback>VR</AvatarFallback>
-              <AvatarImage src="https://github.com/hayatojpg.png" />
-            </Avatar>
-            <p className="leading-relaxed">
-              <span className="block font-bold text-slate-700">Human:</span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Obcaecati, id. Iste expedita accusamus dolor, quo porro nemo quae
-              fugiat earum nisi magnam aliquid voluptatem voluptas odio iusto
-              rerum similique autem.
-            </p>
-          </div>
-          <div className="flex gap-3 text-slate-800 text-sm">
-            <Avatar>
-              <AvatarFallback>VL</AvatarFallback>
-              <AvatarImage src="https://github.com/vercel.png" />
-            </Avatar>
-            <p className="leading-relaxed">
-              <span className="block font-bold text-slate-700">AI:</span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Obcaecati, id. Iste expedita accusamus dolor, quo porro nemo quae
-              fugiat earum nisi magnam aliquid voluptatem voluptas odio iusto
-              rerum similique autem.
-            </p>
-          </div>
-        </CardContent>
-        <CardFooter className="space-x-2">
-          <Input placeholder="How can I help you?" />
-          <Button type="submit">Submit</Button>
-        </CardFooter>
-      </Card>
+      <Chat />
     </div>
   );
 }
---
+++ src/components/Chat.tsx
@@ -0,0 +1,67 @@
+'use client';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { useChat } from 'ai/react';
+import { ScrollArea } from './ui/scroll-area';
+
+export function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    api: '/api/chat',
+  });
+
+  return (
+    <Card className="w-[440px]">
+      <CardHeader>
+        <CardTitle>Chat AI</CardTitle>
+        <CardDescription>
+          Using Vercel SDK to create a chat bot.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ScrollArea className='h-[600px] w-full pr-4'>
+          {messages.map((message) => (
+            <div key={message.id} className="flex gap-3 text-slate-800 text-sm mt-3">
+              {message.role === 'user' && (
+                <Avatar>
+                  <AvatarFallback>VR</AvatarFallback>
+                  <AvatarImage src="https://github.com/hayatojpg.png" />
+                </Avatar>
+              )}
+              {message.role === 'assistant' && (
+                <Avatar>
+                  <AvatarFallback>AI</AvatarFallback>
+                  <AvatarImage src="https://github.com/vercel.png" />
+                </Avatar>
+              )}
+              <p className="leading-relaxed">
+                <span className="block font-bold text-slate-700">
+                  {message.role === 'user' ? 'Human' : 'AI'}:
+                </span>
+                {message.content}
+              </p>
+            </div>
+          ))}
+        </ScrollArea>
+      </CardContent>
+      <CardFooter>
+        <form className="w-full flex gap-2" onSubmit={handleSubmit}>
+          <Input
+            placeholder="How can I help you?"
+            value={input}
+            onChange={handleInputChange}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </CardFooter>
+    </Card>
+  );
+}
